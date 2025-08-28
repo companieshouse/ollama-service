2605264fc4cfababd29fb5176f4642fcd97ba87a
mchhatralia-ch <mchhatralia@companieshouse.gov.uk>
2025-08-28 10:11:34 +0100

    rand-506 set the target path for the service to /ollama-service on the alb

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -6,7 +6,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0.0, < 6.0.0"
-#     version = "~> 4.54.0"
+      #     version = "~> 4.54.0"
     }
     vault = {
       source  = "hashicorp/vault"
@@ -14,7 +14,7 @@ terraform {
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0"  # Specify an appropriate version constraint
+      version = "~> 3.0" # Specify an appropriate version constraint
     }
   }
 }
@@ -60,7 +60,7 @@ module "ecs-service" {
   ecs_cluster_id          = data.aws_ecs_cluster.ecs_cluster.id
   task_execution_role_arn = data.aws_iam_role.ecs_cluster_iam_role.arn
   task_role_arn           = aws_iam_role.ecs_task_role.arn
-  
+
   # Load balancer configuration
   lb_listener_arn           = data.aws_lb_listener.rand_lb_listener.arn
   lb_listener_rule_priority = local.lb_listener_rule_priority
@@ -116,3 +116,36 @@ resource "aws_security_group" "ecs_sg" {
   description = "Security group for ECS service"
   vpc_id      = data.aws_vpc.vpc.id
 }
+
+resource "aws_lb_target_group" "ollama_target_group" {
+  name     = "ollama-target-group"
+  port     = 11434
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "ollama_rule" {
+  listener_arn = data.aws_lb_listener.rand_lb_listener.arn
+  priority     = 1000
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ollama_target_group.arn
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/ollama-service/*"]
+  }
+}
+
