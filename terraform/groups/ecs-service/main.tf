c486eb10d1ad555e47ce4bad671bfe25f39b226c
mchhatralia-ch <mchhatralia@companieshouse.gov.uk>
2025-08-28 10:15:58 +0100

    rand-506 refactor code and set the target path for the service to /ollama-service

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -5,8 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0, < 6.0.0"
-      #     version = "~> 4.54.0"
+      version = ">= 5.0.0, < 6.0.0"     
     }
     vault = {
       source  = "hashicorp/vault"
