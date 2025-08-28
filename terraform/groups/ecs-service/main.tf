8e4cb65f41e802638be4b0110a0bd3907f7e12e1
mchhatralia-ch <144021977+mchhatralia-ch@users.noreply.github.com>
2025-08-28 10:31:16 +0100

    Update terraform/groups/ecs-service/main.tf
    Co-authored-by: HarryBosworth <68226305+HarryBosworth@users.noreply.github.com>

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -117,7 +117,7 @@ resource "aws_security_group" "ecs_sg" {
 }
 
 resource "aws_lb_target_group" "ollama_target_group" {
-  name     = "ollama-target-group"
+  name     = "${local.service_name}-${var.environment}-target-group"
   port     = 11434
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.vpc.id
