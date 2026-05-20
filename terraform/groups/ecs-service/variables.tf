c5b8b838cd3c29cf0d689bb696aca80cccbdcd55
mchhatralia-ch <mchhatralia@companieshouse.gov.uk>
2026-05-20 08:41:59 +0100

    rand-506 Increase cpu and memory to avoid timeout issues

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -45,12 +45,12 @@ variable "desired_task_count" {
 variable "required_cpus" {
   type        = number
   description = "The required cpu resource for this service. 1024 here is 1 vCPU"
-  default     = 2048 # 1024 is the minimum recommended for running Ollama models, works well with 2048
+  default     = 16384 # 1024 is the minimum recommended for running Ollama models, works well with 2048
 }
 variable "required_memory" {
   type        = number
   description = "The required memory for this service"
-  default     = 6144 # 4096 is the minimum recommended for running Ollama models, works well with 6144
+  default     = 32768 # 4096 is the minimum recommended for running Ollama models, works well with 6144
 }
 
 variable "mount_points" {
